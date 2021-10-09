b9a706959a38f1afe347be916dfe55ea4e86c570
Martin Hermida <hello@martinhermida.me>
2021-10-10 00:00:12 +0200

    Fix timetable breaking on iOS 15

--- Corubus/View/LineTimetable.swift
+++ Corubus/View/LineTimetable.swift
@@ -5,14 +5,14 @@ struct LineTimetable: View {
 
     var body: some View {
         List(Array(self.timetable).sorted(by: { Int($0.0)! < Int($1.0)! }), id: \.self.0) { hourKey, hours in
-            Text(hourKey)
-                .font(.subheadline)
-                .fontWeight(.bold)
-                .frame(width: 40)
-
-            Spacer().frame(width: 15)
-
             HStack {
+                Text(hourKey)
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .frame(width: 40)
+
+                Spacer().frame(width: 15)
+                
                 ForEach(0 ..< hours.count, id: \.self) { i in
                     Text(hours[i])
                         .font(.subheadline)
